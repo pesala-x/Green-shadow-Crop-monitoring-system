82ba5140d0ab143f406d870d49b537b1fe6e8960
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-15 20:26:25 +0530

    initial commit

---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/CropMonitorApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.pesalax.cropmonitorapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/pesalax/cropmonitorapplication/CropMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.pesalax.cropmonitorapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
