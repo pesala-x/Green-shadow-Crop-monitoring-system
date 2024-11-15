d2a753059422533f75a1cc2c34dc375595ec9364
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-15 20:38:14 +0530

    make project main structures.

--- src/main/java/lk/ijse/pesalax/cropmonitorapplication/CropMonitorApplication.java
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/CropMonitorApplication.java
@@ -1,13 +1,21 @@
 package lk.ijse.pesalax.cropmonitorapplication;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
+@EnableWebSecurity
 public class CropMonitorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CropMonitorApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper mapper() {return new ModelMapper();}
 }
