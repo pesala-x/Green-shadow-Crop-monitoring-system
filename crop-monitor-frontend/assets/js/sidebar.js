ff1199e06accac22a491c8adae7fb8f7256a1ef3
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-19 22:59:07 +0530

    make togel sidebar content transition issue.

--- crop-monitor-frontend/assets/js/sidebar.js
+++ crop-monitor-frontend/assets/js/sidebar.js
@@ -1,6 +1,14 @@
 document.getElementById("menu-icon").addEventListener("click", function () {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("collapsed");
+
+  const content = document.getElementById("content");
+  if (sidebar.classList.contains("collapsed")) {
+    content.style.marginLeft = "70px";
+  } else {
+    content.style.marginLeft = "260px";
+  }
+
   const isCollapsed = sidebar.classList.contains("collapsed");
   this.setAttribute("aria-expanded", !isCollapsed);
   this.classList.toggle("fa-bars");
