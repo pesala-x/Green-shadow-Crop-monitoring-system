9796a1ea33ce08dcae2d32e032e014b9fafd35e7
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-25 23:19:31 +0530

    field name and field code load to crop form fieldDropdown.

--- crop-monitor-frontend/assets/js/crop.js
+++ crop-monitor-frontend/assets/js/crop.js
@@ -1,14 +1,37 @@
-// crop code
+// generate crop code
 function generateCropCode() {
   const code = "C-" + Math.floor(1000 + Math.random() * 900);
-  document.getElementById("cropCode").value = code;
+  $("#cropCode").val(code);
 }
 
 $(document).ready(function () {
   generateCropCode();
+  loadFields();
 
-  $("#clearBtn").on("click", function () {
-    generateCropCode();
-    $("#cropForm")[0].reset();
-  });
+  // Load field to crop dropdown
+  function loadFields() {
+    $.ajax({
+      url: "http://localhost:5050/crop-monitor/api/v1/fields/allFields", // Endpoint to fetch fields
+      method: "GET",
+      success: function (fields) {
+        $("#field")
+          .empty()
+          .append("<option disabled selected>Select Field</option>");
+
+        fields.forEach((field) => {
+          $("#field").append(
+            new Option(
+              `${field.fieldCode} - ${field.fieldName}`,
+              field.fieldCode
+            )
+          );
+        });
+      },
+      error: function (xhr) {
+        console.error("Failed to load fields:", xhr.responseText);
+      },
+    });
+  }
+
+
 });
