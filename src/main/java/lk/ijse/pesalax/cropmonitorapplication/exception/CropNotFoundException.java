a073e69447f9b2cf7fdecd892485074d5a358736
Pesala winodith <gkpesalawinodith@gmail.com>
2024-11-26 06:31:01 +0530

    crop service layer implement with exception handling.

---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/exception/CropNotFoundException.java
@@ -0,0 +1,6 @@
+package lk.ijse.pesalax.cropmonitorapplication.exception;
+
+public class CropNotFoundException extends RuntimeException{
+    public CropNotFoundException(String message) {
+    }
+}
---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/CropService.java
@@ -0,0 +1,13 @@
+package lk.ijse.pesalax.cropmonitorapplication.service;
+
+import lk.ijse.pesalax.cropmonitorapplication.dto.impl.CropDTO;
+
+import java.util.List;
+
+public interface CropService {
+    void saveCrop(CropDTO cropDTO);
+    List<CropDTO> searchCrops(String cropCode, String cropCommonName);
+    List<CropDTO> getAllCrops();
+    void deleteCrop(String cropCode);
+    void updateCrop(String cropCode, CropDTO cropDTO);
+}
