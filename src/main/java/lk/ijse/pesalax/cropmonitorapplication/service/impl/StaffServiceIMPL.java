4d22df04c73823b3ecfb97508e5fb6a2c64ff874
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-02 10:25:24 +0530

    Staff save in backend implemented.

--- src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/impl/StaffServiceIMPL.java
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/impl/StaffServiceIMPL.java
@@ -1,7 +1,13 @@
 package lk.ijse.pesalax.cropmonitorapplication.service.impl;
 
+import lk.ijse.pesalax.cropmonitorapplication.dao.StaffDAO;
+import lk.ijse.pesalax.cropmonitorapplication.dao.VehicleDAO;
 import lk.ijse.pesalax.cropmonitorapplication.dto.impl.StaffDTO;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.Staff;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.Vehicle;
+import lk.ijse.pesalax.cropmonitorapplication.exception.DataPersistException;
 import lk.ijse.pesalax.cropmonitorapplication.service.StaffService;
+import lk.ijse.pesalax.cropmonitorapplication.util.Mapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,9 +17,24 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class StaffServiceIMPL implements StaffService {
+    private final StaffDAO staffDAO;
+    private final VehicleDAO vehicleDAO;
+    private final Mapping mapping;
+
     @Override
     public void saveStaff(StaffDTO staffDTO) {
-
+        Vehicle vehicle = vehicleDAO.findById(staffDTO.getVehicleCode())
+                .orElseThrow(() -> new DataPersistException("Invalid Vehicle code"));
+        Staff staff = mapping.convertToStaff(staffDTO);
+        staff.setVehicle(vehicle);
+        Staff savedStaff = staffDAO.save(staff);
+        try {
+            if (savedStaff == null) {
+                throw new DataPersistException("Can't save Staff");
+            }
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
