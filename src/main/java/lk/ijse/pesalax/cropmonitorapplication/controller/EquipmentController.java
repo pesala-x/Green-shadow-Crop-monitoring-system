73c7255337f87be24a50600a1a603d5fb255e9e3
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-02 10:52:43 +0530

    Equipment save backend implemented.

--- src/main/java/lk/ijse/pesalax/cropmonitorapplication/controller/EquipmentController.java
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/controller/EquipmentController.java
@@ -1,10 +1,13 @@
 package lk.ijse.pesalax.cropmonitorapplication.controller;
 
+import lk.ijse.pesalax.cropmonitorapplication.dto.impl.EquipmentDTO;
+import lk.ijse.pesalax.cropmonitorapplication.exception.DataPersistException;
 import lk.ijse.pesalax.cropmonitorapplication.service.EquipmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/equipment")
@@ -13,4 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class EquipmentController {
     private final EquipmentService equipmentService;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO equipment) {
+        if (equipment == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+            try {
+                equipmentService.saveEquipment(equipment);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            } catch (DataPersistException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } catch (Exception e) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
 }
--- src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/impl/EquipmentServiceIMPL.java
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/impl/EquipmentServiceIMPL.java
@@ -1,7 +1,15 @@
 package lk.ijse.pesalax.cropmonitorapplication.service.impl;
 
+import lk.ijse.pesalax.cropmonitorapplication.dao.EquipmentDAO;
+import lk.ijse.pesalax.cropmonitorapplication.dao.FieldDAO;
+import lk.ijse.pesalax.cropmonitorapplication.dao.StaffDAO;
 import lk.ijse.pesalax.cropmonitorapplication.dto.impl.EquipmentDTO;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.Equipment;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.Field;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.Staff;
+import lk.ijse.pesalax.cropmonitorapplication.exception.DataPersistException;
 import lk.ijse.pesalax.cropmonitorapplication.service.EquipmentService;
+import lk.ijse.pesalax.cropmonitorapplication.util.Mapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,14 +19,35 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class EquipmentServiceIMPL implements EquipmentService {
+    private final EquipmentDAO equipmentDAO;
+    private final FieldDAO fieldDAO;
+    private final StaffDAO staffDAO;
+    private final Mapping mapping;
+
     @Override
     public void saveEquipment(EquipmentDTO equipmentDTO) {
+        Field field = fieldDAO.findById(equipmentDTO.getFieldCode())
+                .orElseThrow(() -> new DataPersistException("Invalid Field code"));
+        Staff staff = staffDAO.findById(equipmentDTO.getId())
+                .orElseThrow(() -> new DataPersistException("Invalid Staff code"));
 
+        Equipment equipment = mapping.convertToEquipment(equipmentDTO);
+
+        equipment.setField(field);
+        equipment.setStaff(staff);
+        Equipment savedEquipment = equipmentDAO.save(equipment);
+        try {
+            if (savedEquipment == null) {
+                throw new DataPersistException("Can't save Equipment");
+            }
+        } catch (DataPersistException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<EquipmentDTO> getAllEquipment() {
-        return null;
+        return List.of();
     }
 
     @Override
@@ -33,6 +62,6 @@ public class EquipmentServiceIMPL implements EquipmentService {
 
     @Override
     public List<EquipmentDTO> searchEquipment(String searchTerm) {
-        return null;
+        return List.of();
     }
 }
