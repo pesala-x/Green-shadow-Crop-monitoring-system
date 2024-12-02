8b3b66699d0827466ebfa958154b6067e6209273
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-02 21:19:50 +0530

    load field to equipment form dropdown in frontend

--- crop-monitor-frontend/assets/js/equipment.js
+++ crop-monitor-frontend/assets/js/equipment.js
@@ -1,14 +1,30 @@
-// generate equipment ID
-function generateEquipmentId() {
-  const code = "E-" + Math.floor(1000 + Math.random() * 900);
-  document.getElementById("equipmentId").value = code;
-}
-
 $(document).ready(function () {
-  generateEquipmentId();
+  generateEquipmentID();
+  loadStaffDropdown();
+  loadFieldDropdown();
 
-  $("#clearBtn").on("click", function () {
-    generateEquipmentId();
-    $("#equipmentForm")[0].reset();
-  });
+  // Generate Equipment ID
+  function generateEquipmentID() {
+    const id = "E-" + Math.floor(1000 + Math.random() * 9000);
+    $("#equipmentId").val(id);
+  }
+
+  // Load Field Dropdown
+  function loadFieldDropdown() {
+    $.ajax({
+      url: "http://localhost:5050/crop-monitor/api/v1/fields/allFields",
+      method: "GET",
+      success: function (fieldList) {
+        fieldList.forEach((field) => {
+          $("#assignedField").append(
+            `<option value="${field.fieldCode}">${field.fieldCode} - ${field.fieldName}</option>`
+          );
+        });
+      },
+      error: function () {
+        alert("Failed to load field data.");
+      },
+    });
+  }
+
 });
