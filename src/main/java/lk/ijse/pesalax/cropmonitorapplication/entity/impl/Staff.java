8ad231af2bfe1952e589e131e5a57aab48d2484c
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-02 07:10:34 +0530

    update staff address in backend

--- src/main/java/lk/ijse/pesalax/cropmonitorapplication/entity/impl/Staff.java
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/entity/impl/Staff.java
@@ -24,12 +24,16 @@ public class Staff implements SuperEntity {
     private String gender;
     private Date joinedDate;
     private Date dob;
-    private String address;
     private String role;
+    private String addressLine01;
+    private String addressLine02;
+    private String addressLine03;
+    private String addressLine04;
+    private String addressLine05;
     private String contactNo;
     private String email;
     @ManyToOne
-    @JoinColumn(name = "vehicleCode", nullable = false)
+    @JoinColumn(name = "vehicleCode",nullable = false)
     private Vehicle vehicle;
     @OneToMany(mappedBy = "staff", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<FieldStaffAssignment> assignments = new ArrayList<>();
