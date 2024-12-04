4f92da4dd7274dc7e6c9f8df1a5d7aa2e0805d6c
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-04 08:09:49 +0530

    Load staff & crop  into MonitoringLog dropdown in frontend

--- crop-monitor-frontend/assets/js/monitoringLog.js
+++ crop-monitor-frontend/assets/js/monitoringLog.js
@@ -7,6 +7,8 @@ function generateLogCode() {
 $(document).ready(function () {
   generateLogCode();
   loadFields();
+  loadCrops();
+  loadStaff();
 
   // Load fields into dropdown
   function loadFields() {
@@ -28,6 +30,51 @@ $(document).ready(function () {
       },
       error: function () {
         alert("Failed to load fields.");
+      },
+    });
+  }
+
+  // Load crops into dropdown
+  function loadCrops() {
+    $.ajax({
+      url: "http://localhost:5050/crop-monitor/api/v1/crops/allcrops",
+      method: "GET",
+      success: function (crops) {
+        $("#cropSelect")
+          .empty()
+          .append("<option disabled selected>Select Crop</option>");
+        crops.forEach((crop) => {
+          $("#cropSelect").append(
+            new Option(
+              `${crop.cropCode} - ${crop.cropCommonName}`,
+              crop.cropCode
+            )
+          );
+        });
+      },
+      error: function () {
+        alert("Failed to load crops.");
+      },
+    });
+  }
+
+  // Load staff into dropdown
+  function loadStaff() {
+    $.ajax({
+      url: "http://localhost:5050/crop-monitor/api/v1/staff/allstaff",
+      method: "GET",
+      success: function (staffList) {
+        $("#staffSelect")
+          .empty()
+          .append("<option disabled selected>Select Staff</option>");
+        staffList.forEach((staff) => {
+          $("#staffSelect").append(
+            new Option(`${staff.id} - ${staff.firstName}`, staff.id)
+          );
+        });
+      },
+      error: function () {
+        alert("Failed to load staff.");
       },
     });
   }
