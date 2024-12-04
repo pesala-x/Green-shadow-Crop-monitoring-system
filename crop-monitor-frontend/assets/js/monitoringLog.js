708aa3b2b4dd1920ce3beb8b42f1d750f3ac8a1e
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-04 08:05:20 +0530

    Load fields into MonitoringLog dropdown in frontend

--- crop-monitor-frontend/assets/js/monitoringLog.js
+++ crop-monitor-frontend/assets/js/monitoringLog.js
@@ -6,4 +6,29 @@ function generateLogCode() {
 
 $(document).ready(function () {
   generateLogCode();
+  loadFields();
+
+  // Load fields into dropdown
+  function loadFields() {
+    $.ajax({
+      url: "http://localhost:5050/crop-monitor/api/v1/fields/allFields",
+      method: "GET",
+      success: function (fields) {
+        $("#fieldSelect")
+          .empty()
+          .append("<option disabled selected>Select Field</option>");
+        fields.forEach((field) => {
+          $("#fieldSelect").append(
+            new Option(
+              `${field.fieldCode} - ${field.fieldName}`,
+              field.fieldCode
+            )
+          );
+        });
+      },
+      error: function () {
+        alert("Failed to load fields.");
+      },
+    });
+  }
 });
