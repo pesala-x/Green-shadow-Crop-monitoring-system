1c6e262276fb33570bd108d07691b2e8b1326779
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-05 11:06:42 +0530

    implement AuthenticationService

---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/AuthenticationService.java
@@ -0,0 +1,13 @@
+package lk.ijse.pesalax.cropmonitorapplication.service;
+
+import lk.ijse.pesalax.cropmonitorapplication.auth.request.SignInRequest;
+import lk.ijse.pesalax.cropmonitorapplication.auth.request.SignUpRequest;
+import lk.ijse.pesalax.cropmonitorapplication.auth.response.JWTAuthResponse;
+
+public interface AuthenticationService {
+    JWTAuthResponse signIn(SignInRequest signInRequest);
+
+    JWTAuthResponse signUp(SignUpRequest signUpRequest);
+
+    JWTAuthResponse refreshToken(String accessToken);
+}
