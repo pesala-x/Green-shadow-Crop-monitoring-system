10d53f2d2063771d1fa1693dc72dc51c6883e96c
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-05 14:25:15 +0530

    access control for field

--- src/main/java/lk/ijse/pesalax/cropmonitorapplication/controller/FieldController.java
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/controller/FieldController.java
@@ -12,18 +12,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping(value = "api/v1/fields")
+@RequiredArgsConstructor
 @CrossOrigin(origins = "http://127.0.0.1:5501")
 public class FieldController {
     private final FieldService fieldService;
 
+    @PreAuthorize("hasAnyRole('MANAGER', 'SCIENTIST')")
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<FieldErrorResponse> saveField(@RequestParam("fieldCode") String fieldCode, @RequestParam("fieldName") String fieldName, @RequestParam("fieldLocation") String fieldLocation, @RequestParam("extentSize") Double extentSize, @RequestParam("fieldImage1") MultipartFile fieldImage1, @RequestParam("fieldImage2") MultipartFile fieldImage2, HttpServletRequest request // Add this to log the request content type
 
@@ -83,6 +85,7 @@ public class FieldController {
         return new ResponseEntity<>(fields, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('MANAGER', 'SCIENTIST')")
     @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, value = "/{fieldCode}")
     public ResponseEntity<Void> updateSelectedField(@PathVariable("fieldCode") String fieldCode, @RequestParam(value = "fieldName", required = false) String fieldName, @RequestParam(value = "fieldLocation", required = false) String fieldLocation, @RequestParam(value = "extentSize", required = false) Double extentSize, @RequestParam(value = "fieldImage1", required = false) MultipartFile fieldImage1, @RequestParam(value = "fieldImage2", required = false) MultipartFile fieldImage2) {
         try {
@@ -107,6 +110,7 @@ public class FieldController {
         }
     }
 
+    @PreAuthorize("hasAnyRole('MANAGER', 'SCIENTIST')")
     @DeleteMapping(value = "/{code}")
     public ResponseEntity<Void> deleteSelectedField(@PathVariable("code") String code) {
         try {
