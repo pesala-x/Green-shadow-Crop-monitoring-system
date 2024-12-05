9216cd6a4b7509c0f82bf50febad0bfd7a12685b
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-05 09:55:31 +0530

    signIn & signUp requests implemented

---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/auth/request/SignUpRequest.java
@@ -0,0 +1,17 @@
+package lk.ijse.pesalax.cropmonitorapplication.auth.request;
+
+import lk.ijse.pesalax.cropmonitorapplication.util.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SignUpRequest {
+    private String email;
+    private String password;
+    private Role role;
+}
