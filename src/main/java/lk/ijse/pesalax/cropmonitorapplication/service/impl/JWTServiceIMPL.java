4a32ec997096d2fae0fe5ddc328be9837227fb22
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-05 18:46:13 +0530

    isTokenValid method update

--- src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/impl/JWTServiceIMPL.java
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/impl/JWTServiceIMPL.java
@@ -42,11 +42,22 @@ public class JWTServiceIMPL implements JWTService {
         return accessToken;
     }
 
+    //    @Override
+//    public boolean isTokenValid(String token, UserDetails userDetails) {
+//        String subject = extractClaims(token, Claims::getSubject);
+//        return subject.equals(userDetails.getUsername()) && !isExpired(token);
+//    }
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
-        String subject = extractClaims(token, Claims::getSubject);
-        return subject.equals(userDetails.getUsername()) && !isExpired(token);
+        try {
+            String subject = extractClaims(token, Claims::getSubject);
+            return subject.equals(userDetails.getUsername()) && !isExpired(token);
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+            // Token is expired
+            return false;
+        }
     }
+
 
     private Key getSignKey() {
         byte[] bytes = Decoders.BASE64.decode(jwtKey); //decode the base64-encoded jwt
