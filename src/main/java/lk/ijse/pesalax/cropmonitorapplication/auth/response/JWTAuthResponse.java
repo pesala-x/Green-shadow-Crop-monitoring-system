79ed52e44916fef6eb6415fce86535c3784d4e7c
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-05 09:51:13 +0530

    jwt response implemented

---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/auth/response/JWTAuthResponse.java
@@ -0,0 +1,15 @@
+package lk.ijse.pesalax.cropmonitorapplication.auth.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JWTAuthResponse {
+    private String token;
+    private String refreshToken;
+}
