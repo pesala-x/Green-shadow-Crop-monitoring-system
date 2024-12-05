6ac466fdc9042d598e56644320a4df31a359c662
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-05 11:05:19 +0530

    jwt serviceIMPL & service implemented

---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/JWTService.java
@@ -0,0 +1,14 @@
+package lk.ijse.pesalax.cropmonitorapplication.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JWTService {
+    String extractUserName(String token);
+
+    String generateToken(UserDetails userDetails);
+
+    boolean isTokenValid(String token, UserDetails userDetails);
+
+    String refreshToken(UserDetails userDetails);
+}
+
