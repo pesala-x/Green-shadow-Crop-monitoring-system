90bf74860e00500d0407fb3807050331efd280d2
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-05 18:58:22 +0530

    implement FieldAssignmentStaffServiceIMPL

---
+++ src/main/java/lk/ijse/pesalax/cropmonitorapplication/service/impl/FieldAssignmentStaffServiceIMPL.java
@@ -0,0 +1,38 @@
+package lk.ijse.pesalax.cropmonitorapplication.service.impl;
+
+import lk.ijse.pesalax.cropmonitorapplication.dao.FieldDAO;
+import lk.ijse.pesalax.cropmonitorapplication.dao.FieldStaffAssignmentDAO;
+import lk.ijse.pesalax.cropmonitorapplication.dao.StaffDAO;
+import lk.ijse.pesalax.cropmonitorapplication.dto.impl.FieldStaffAssignmentDTO;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.Field;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.FieldStaffAssignment;
+import lk.ijse.pesalax.cropmonitorapplication.entity.impl.Staff;
+import lk.ijse.pesalax.cropmonitorapplication.service.FieldAssignmentStaffService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FieldAssignmentStaffServiceIMPL implements FieldAssignmentStaffService {
+    private final FieldStaffAssignmentDAO fieldStaffAssignmentDAO;
+    private final FieldDAO fieldDAO;
+    private final StaffDAO staffDAO;
+
+    @Override
+    public void saveAssignment(FieldStaffAssignmentDTO fieldStaffAssignmentDTO) {
+        FieldStaffAssignment fieldStaffAssignment = new FieldStaffAssignment();
+        Field field = fieldDAO.findById(fieldStaffAssignmentDTO.getFieldCode())
+                .orElseThrow(() -> new RuntimeException("Field not found"));
+
+        Staff staff = staffDAO.findById(fieldStaffAssignmentDTO.getStaffId())
+                .orElseThrow(() -> new RuntimeException("Staff not found"));
+        fieldStaffAssignment.setField(field);
+        fieldStaffAssignment.setStaff(staff);
+        fieldStaffAssignment.setAssignedRole(fieldStaffAssignmentDTO.getAssignedRole());
+        fieldStaffAssignment.setAssignmentDate(fieldStaffAssignmentDTO.getAssignmentDate());
+
+        fieldStaffAssignmentDAO.save(fieldStaffAssignment);
+    }}
+
